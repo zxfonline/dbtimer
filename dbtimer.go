cd9479ed12f5d61b7ac66d5d20f84ea7d8d65d02
zxfonline <zxfonline@sina.com>
2018-09-07 17:41:34 +0800

    修复异步处理bug

--- dbtimer.go
+++ dbtimer.go
@@ -26,22 +26,31 @@ var (
 
 	logger *golog.Logger = golog.New("DBTimer")
 
-	timer_map map[int64]*RTimer
-	_timer_lock sync.RWMutex
+	timer_map   map[int64]*RTimer
+	_timer_lock sync.Mutex
 
 	timer_ch      chan int64
 	nextTimerId   int64
 	loadstate     int32
 	stopD         chanutil.DoneChan
 	tsexcutor     taskexcutor.Excutor
-	event_trigger map[Action]*taskexcutor.TaskService
+	event_trigger map[Action]*TimerHandler
 )
 
 func init() {
 	timer_map = make(map[int64]*RTimer)
 	timer_ch = make(chan int64, MAX_TIMER_CHAN_SIZE)
 	stopD = chanutil.NewDoneChan()
-	event_trigger = make(map[Action]*taskexcutor.TaskService)
+	event_trigger = make(map[Action]*TimerHandler)
+}
+
+type TimerHandler struct {
+	callback taskexcutor.CallBack
+	ID       interface{}
+}
+
+func NewTimerHandler(callback taskexcutor.CallBack) *TimerHandler {
+	return &TimerHandler{callback: callback}
 }
 
 //消息执行事件
@@ -55,11 +64,13 @@ type Msg struct {
 
 //事件
 type RTimer struct {
-	Receiver int64 //接受者表示符
+	Receiver int64 //接受者标识符
 	Msg      Msg   //存储的数据
 	expired  int64 // 到期的 UTC时间 毫秒
 	//定时任务唯一id
 	TimerId int64
+	//particleTimer id
+	Ptid int64
 }
 
 type TimerInfo struct {
@@ -69,7 +80,7 @@ type TimerInfo struct {
 
 //存档定时器数据 Marshal/unmarshal timers
 type DBTimer struct {
-	Receiver int64 //接受者表示符
+	Receiver int64 //接受者标识符
 	Expired  int64 // 到期的 UTC时间 毫秒
 	Id       int64 //定时器唯一id
 	Data     Msg   //存储的数据
@@ -86,7 +97,11 @@ func Close() {
 	stopD.SetDone()
 }
 
-// 创建延时执行定时器 delay 毫秒 返回定时器唯一id 当返回0表示添加失败
+/**
+*创建延时执行定时器 delay 毫秒 返回定时器唯一id 当返回0表示添加失败
+* 注意：存档导出只会导出设置了userId>0标识符的任务
+* 其他任务统一当临时任务处理，不做存档
+ */
 func CreateTimer(userId int64, delay int64, msg Msg) int64 {
 	if atomic.LoadInt32(&loadstate) != 1 {
 		logger.Warnf("timer no open or closed, msg:%+v", msg)
@@ -104,23 +119,22 @@ func CreateTimer(userId int64, delay int64, msg Msg) int64 {
 	if delay < 0 {
 		delay = 0
 	}
-	expired := particletimer.Current() + delay
-	tid := atomic.AddInt64(&nextTimerId, 1)
-
-	particletimer.Add(tid, expired, timer_ch)
 	_timer_lock.Lock()
-	timer_map[tid] = &RTimer{
+	defer _timer_lock.Unlock()
+	nextTimerId++
+	rtimer := &RTimer{
 		Receiver: userId,
-		expired:  expired,
+		expired:  particletimer.Current() + delay,
 		Msg:      msg,
-		TimerId:  tid,
+		TimerId:  nextTimerId,
 	}
-	_timer_lock.Unlock()
-	return tid
+	timer_map[rtimer.TimerId] = rtimer
+	rtimer.Ptid = particletimer.Add(rtimer.TimerId, rtimer.expired, timer_ch)
+	return rtimer.TimerId
 }
 
 //注册定时事件执行函数 handler默认第一个参数将由定时器赋值为 *dbtimer.RTimer
-func RegistTimerHander(action Action, handler *taskexcutor.TaskService) {
+func RegistTimerHander(action Action, handler *TimerHandler) {
 	if handler == nil {
 		panic(errors.New("illegal handler error"))
 	}
@@ -128,7 +142,8 @@ func RegistTimerHander(action Action, handler *taskexcutor.TaskService) {
 		panic(fmt.Errorf("repeat regist handler error,action=%d handler=%+v", action, handler))
 	}
 	event_trigger[action] = handler
-	logger.Infof("Regist action=%d handler=%+v", action, handler)
+	handler.ID = action
+	logger.Infof("regist action=%d", action)
 }
 
 /*加载定时器数据，返回开启定时器函数
@@ -145,16 +160,18 @@ func StartTimers(data []byte, excutor taskexcutor.Excutor) func() {
 		Timers: make([]DBTimer, 0),
 		NextId: 0,
 	}
-	if data != nil {
+	if data != nil && len(data) > 0 {
 		enc := gob.NewDecoder(bytes.NewReader(data))
 		if err := enc.Decode(info); err != nil {
 			panic(fmt.Errorf("decode timer data error,err:%v", err))
 		}
 	}
 
+	_timer_lock.Lock()
+	nextTimerId = info.NextId
 	// reset next timer id
-	atomic.StoreInt64(&nextTimerId, info.NextId)
 	logger.Infof("load timerNextId=%d", nextTimerId)
+	_timer_lock.Unlock()
 	tsexcutor = excutor
 
 	return func() {
@@ -181,41 +198,49 @@ func working() {
 	}()
 	for q := false; !q; {
 		select {
+		case <-stopD:
+			q = true
 		case id := <-timer_ch:
-			_timer_lock.RLock()
+			_timer_lock.Lock()
 			if ts, ex := timer_map[id]; ex {
-				_timer_lock.RUnlock()
+				_timer_lock.Unlock()
 				//需要玩家手动释放，避免任务执行失败或丢失
 				// CancelTimer(id)
 				if tt, ok := event_trigger[ts.Msg.Action]; ok {
-					tt.AddArgs(0, ts)
-					tsexcutor.Excute(tt)
+					task := taskexcutor.NewTaskService(tt.callback, ts)
+					task.ID = tt.ID
+					tsexcutor.Excute(task)
 				} else {
-					logger.Warnf("no found trigger, timer:%+v", ts)
+					logger.Warnf("timer no found trigger, timer:%+v", ts)
 				}
 			} else {
-				_timer_lock.RUnlock()
+				_timer_lock.Unlock()
 				CancelTimer(id)
+				logger.Warnf("timer no found, timer:%+v", ts)
 			}
-		case <-stopD:
-			q = true
 		}
 	}
 }
 
 func rescheduleTimers(info *TimerInfo) {
 	for _, item := range info.Timers {
-		// reschedule
-		particletimer.Add(item.Id, item.Expired, timer_ch)
-		timer_map[item.Id] = &RTimer{
+		rtimer := &RTimer{
 			Receiver: item.Receiver,
 			Msg:      item.Data,
 			expired:  item.Expired,
 			TimerId:  item.Id,
 		}
+		timer_map[item.Id] = rtimer
+		// reschedule
+		rtimer.Ptid = particletimer.Add(item.Id, item.Expired, timer_ch)
 	}
 }
 
+/**
+* 数据库定时任务导出
+* 注意：只会导出设置了Receiver>0标识符的任务
+* 其他任务统一当临时任务处理，不做存档
+ */
 func DumpTimers() (error, []byte) {
 	_timer_lock.Lock()
 	defer _timer_lock.Unlock()
@@ -223,7 +248,6 @@ func DumpTimers() (error, []byte) {
 		Timers: make([]DBTimer, 0, len(timer_map)),
 		NextId: nextTimerId,
 	}
-
 	for tid, tm := range timer_map {
 		if tm.Receiver > 0 {
 			entry := DBTimer{
@@ -241,7 +265,7 @@ func DumpTimers() (error, []byte) {
 	if err := enc.Encode(info); err != nil {
 		return err, nil
 	}
-	logger.Infof("save timerNextId=%d,size:", nextTimerId, len(info.Timers))
+	logger.Infof("save timerNextId=%d,size:%d", nextTimerId, len(info.Timers))
 	return nil, buffer.Bytes()
 }
 
@@ -251,22 +275,36 @@ func CancelTimer(timerId int64) {
 		return
 	}
 	_timer_lock.Lock()
+	defer _timer_lock.Unlock()
 	if _, present := timer_map[timerId]; !present {
-		_timer_lock.Unlock()
 		return
 	}
 	delete(timer_map, timerId)
-	_timer_lock.Unlock()
 	particletimer.Del(timerId)
 }
 
 //指定定时事件是否存在
-func TimerExist(timerId int64) bool {
+func TimerExist(timerId int64, checkParticle bool) bool {
 	if timerId == 0 {
 		return false
 	}
-	_timer_lock.RLock()
-	_, present := timer_map[timerId]
-	_timer_lock.RUnlock()
+	_timer_lock.Lock()
+	defer _timer_lock.Unlock()
+	rtimer, present := timer_map[timerId]
+	if present && checkParticle {
+		if ok := particletimer.ParticleTimerExist(rtimer.TimerId); !ok {
+			rtimer.Ptid = particletimer.Add(rtimer.TimerId, rtimer.expired, timer_ch)
+			logger.Warnf("particle timer not exits,fix timerId:%d", timerId)
+		}
+	}
 	return present
+}
+
+func GetTimerInfo(timerId int64) *RTimer {
+	if timerId == 0 {
+		return nil
+	}
+	_timer_lock.Lock()
+	defer _timer_lock.Unlock()
+	return timer_map[timerId]
 }
